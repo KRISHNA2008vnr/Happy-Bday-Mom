24704625e14d9f64209287643c2bccf74c0416b5
KRISHNA2008vnr <me2spidey@gmail.com>
2024-05-15 21:21:35 +0530

    Add files via upload

---
+++ Google Sheet.js
@@ -0,0 +1,11 @@
+const scriptURL = 'https://script.google.com/macros/s/AKfycbzdVm6fbU5gqC6dgsDz0gdvN5GOepWPeEFWvSmnoQZGFyAhr7r4UEaAh-7TsAGcyed6Mg/exec'
+
+const form = document.forms['contact-form']
+
+form.addEventListener('submit', e => {
+  e.preventDefault()
+  fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+  .then(response => alert("Thank you! your wishes are saved successfully." ))
+  .then(() => { window.location.reload(); })
+  .catch(error => console.error('Error!', error.message))
+})
